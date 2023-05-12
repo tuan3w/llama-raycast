fd8e0a29c432173132d88ae1718207eaed96af31
tuan3w <tuannd.dev@gmail.com>
2023-05-12 15:38:23 +0700

    chore: cleanup code

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,9 @@
-enum Filter {
-  All = "all",
-  Open = "open",
-  Completed = "completed",
-}
-
 interface AICommand {
   id: string;
   icon?: string;
   title: string;
   instruction: string;
 }
-
-export { Filter };
-export type { AICommand };
 
 type State = {
   isLoading: boolean;
